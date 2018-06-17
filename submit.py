400ef144464ee93b282652d5cda8cc72b681c60a
Thom Wiggers <thom@thomwiggers.nl>
2018-06-17 20:02:41 +0200

    initial poc

---
+++ submit.py
@@ -0,0 +1,42 @@
+#!/usr/bin/env python3
+
+from w1 import Manager, Family
+
+from prometheus_client import CollectorRegistry, Gauge, push_to_gateway
+from prometheus_client.exposition import basic_auth_handler
+
+# Prometheus info
+HOST = 'cocytus:9091'
+USERNAME = ''
+PASSWORD = ''
+
+# temp manager
+manager = Manager()
+
+
+def get_temps():
+    temps = {}
+    for slave in manager.slaves(family=Family.THERM):
+        degrees = slave.celsius
+        if 10 < degrees < 40:
+            temps[slave.name] = degrees
+        print("Temp {}: {}".format(slave.name, degrees))
+    return temps
+
+
+def my_auth_handler(*args, **kwargs):
+    kwargs['username'] = USERNAME
+    kwargs['password'] = PASSWORD
+    return basic_auth_handler(*args, **kwargs)
+
+
+def submit_temps(temps):
+    registry = CollectorRegistry()
+    for name, temp in temps.items():
+        name = name.replace('28-', '')
+        g = Gauge('temperature_{}'.format(name), 'Degrees Celsius', registry=registry)
+        g.set(temp)
+    push_to_gateway(HOST, job='pushgateway', registry=registry, handler=my_auth_handler)
+
+if __name__ == "__main__":
+    submit_temps(get_temps())
