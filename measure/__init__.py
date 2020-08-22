7a909ad473bfa2a1750d4a7020bc33f4f3e53f1b
Thom Wiggers <thom@thomwiggers.nl>
2020-08-22 15:03:11 +0200

    use a pull model

--- measure/__init__.py
+++ measure/__init__.py
@@ -1,16 +1,12 @@
 #!/usr/bin/env python3
 
+import sys
 import time
 
 from w1 import Manager, Family
 
-from prometheus_client import CollectorRegistry, Gauge, push_to_gateway
+from prometheus_client import CollectorRegistry, Gauge, start_http_server
 from prometheus_client.exposition import basic_auth_handler
-
-# Prometheus info
-HOST = 'cocytus:9091'
-USERNAME = ''
-PASSWORD = ''
 
 # temp manager
 manager = Manager()
@@ -26,35 +22,26 @@ def get_temps():
     return temps
 
 
-def my_auth_handler(*args, **kwargs):
-    kwargs['username'] = USERNAME
-    kwargs['password'] = PASSWORD
-    return basic_auth_handler(*args, **kwargs)
-
-
-def submit_temps(temps):
+def register_temps():
     registry = CollectorRegistry()
     found = False
-    for name, temp in temps.items():
-        name = name.replace('28-', '')
-        g = Gauge('temperature_{}'.format(name),
-                  'Degrees Celsius', registry=registry)
-        g.set(temp)
-        found = True
-    if not found:
-        print("No sensors found")
-    try:
-        push_to_gateway(
-            HOST, job='pushgateway',
-            registry=registry, handler=my_auth_handler)
-    except:
-        pass
+    gauges = {}
+    while True:
+        for name, temp in get_temps().items():
+            name = name.replace('28-', '')
+            if name not in gauges:
+                gauges[name] = Gauge('temperature_{}'.format(name), 'Degrees Celsius')
+            gauges[name].set(temp)
+            found = True
+        if not found:
+            print("No sensors found")
+            sys.exit(1)
+        time.sleep(10)
 
 
 def run():
-    while True:
-        submit_temps(get_temps())
-        time.sleep(30)
+    start_http_server(9101)
+    register_temps()
 
 
 if __name__ == "__main__":
